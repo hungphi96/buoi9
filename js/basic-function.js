b211a3be606ed965dfb605cf0ce5178fc005100d
hungphi96 <hungphi12121996@gmail.com>
2022-08-02 18:23:10 +0700

    hb

---
+++ js/basic-function.js
@@ -0,0 +1,6 @@
+var msg = 'Sign up to receive our newDSADASDetter for 10% off!';
+function updateMessage() { 
+    var el = document.getElementById('message1');
+el.textContent = msg; 
+}
+updateMessage();
